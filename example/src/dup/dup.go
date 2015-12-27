6a4a7e1d959a68c280898310b1f04a1059aaf67a
Mark Little <mlittle@redhat.com>
2015-12-27 20:21:51 +0000

    initial dup code.

---
+++ example/src/dup/dup.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+       "fmt"
+       "os"
+       "bufio"
+)
+
+func main () {
+     counts := make(map[string]int)
+     input := bufio.NewScanner(os.Stdin)
+     
+     for input.Scan() {
+     	 counts[input.Text()]++
+     }
+
+     // NOTE ignore errors from input.Err()
+
+     for line, n := range counts {
+     	 if n > 1 {
+	    fmt.Printf("%d\t%s\n", n, line)
+	 }
+     }
+}
